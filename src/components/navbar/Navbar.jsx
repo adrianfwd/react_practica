329811a6b8537f45f197027a9297f5fd5704661c
adrianfwd <azuniga@fwdcostarica.com>
2024-09-02 15:37:36 -0600

    dkfaosjf

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navbar.css";
+
+export const Navbar = () => {
+  return (
+    <div className="container">
+      <img src="" alt="" className="logo"/>
+
+      <ul>
+        <li>Home</li>
+        <li>services</li>
+        <li>Features</li>
+        <li>About</li>
+        <li>Contacts</li>
+      </ul>
+    </div>
+  );
+};
+export default Navbar;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,5 @@
+function Login() {
+    return <h1>pingona</h1>
+}
+
+export default Login
